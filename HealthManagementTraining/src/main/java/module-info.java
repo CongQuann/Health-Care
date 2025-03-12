6133ee159affe85fbbe2a4bfada5a87e590e559c
Tran Trong Han <hintran6@gmail.com>
2025-03-12 19:30:39 +0700

    tao 2 controller,2 fxml cho bai tap va dinh duong

--- HealthManagementTraining/src/main/java/module-info.java
+++ HealthManagementTraining/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.sixthgroup.healthmanagementtraining {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires java.base;
     opens com.sixthgroup.healthmanagementtraining to javafx.fxml;
     exports com.sixthgroup.healthmanagementtraining;
 }
