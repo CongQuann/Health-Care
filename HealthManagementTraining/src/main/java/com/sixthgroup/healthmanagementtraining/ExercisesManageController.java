733b2051aba68307d865409d896f5e8c06cdaac3
Tran Trong Han <hintran6@gmail.com>
2025-03-14 20:35:48 +0700

    doi ten file

---
+++ HealthManagementTraining/src/main/java/com/sixthgroup/healthmanagementtraining/ExercisesManageController.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sixthgroup.healthmanagementtraining;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author PC
+ */
+public class ExercisesManageController {
+   
+}
