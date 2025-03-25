24440de4414a93e3f86a473be3e4ad06b217c93b
CongQuann <quanpt1610@gmail.com>
2025-03-25 12:44:51 +0700

    add save current user status

--- HealthManagementTraining/src/main/java/com/sixthgroup/healthmanagementtraining/services/Utils.java
+++ HealthManagementTraining/src/main/java/com/sixthgroup/healthmanagementtraining/services/Utils.java
@@ -6,6 +6,7 @@ package com.sixthgroup.healthmanagementtraining.services;
 
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import java.util.prefs.Preferences;
 
 /**
  *
@@ -15,4 +16,21 @@ public class Utils {
     public static Alert getAlert(String content) {
         return new Alert(Alert.AlertType.INFORMATION, content, ButtonType.OK);
     }
+    
+    //current user
+    private static final Preferences prefs = Preferences.userRoot().node("HealthManagementTraining");
+
+    public static void saveUser(String username) {
+        prefs.put("loggedInUser", username);
+    }
+
+    public static String getUser() {
+        return prefs.get("loggedInUser", null);
+    }
+
+    public static void clearUser() {
+        prefs.remove("loggedInUser");
+    }
 }
+
+
